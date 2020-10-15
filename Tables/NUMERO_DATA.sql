39578c8068a94b9438a0ba5e482d2af5e72e5bb7
Pablo Céspedes <pablocespedes@sah.org.py>
2020-10-15 07:49:51 -0300

    add data tables and functions

---
+++ Tables/NUMERO_DATA.sql
@@ -0,0 +1,143 @@
+prompt Importing table NUMERO...
+set feedback off
+set define off
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (100, 'Cien');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (300, 'Trescientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (400, 'Cuatrocientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (500, 'Quinientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (600, 'Seiscientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (700, 'Setecientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (800, 'Ochocientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (900, 'Novecientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (200, 'Doscientos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (10, 'Diez');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (20, 'Veinte');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (30, 'Treinta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (40, 'Cuarenta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (50, 'Cincuenta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (60, 'Sesenta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (70, 'Setenta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (80, 'Ochenta');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (90, 'Noventa');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (1, 'Un');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (2, 'Dos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (3, 'Tres');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (4, 'Cuatro');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (5, 'Cinco');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (6, 'Seis');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (7, 'Siete');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (8, 'Ocho');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (9, 'Nueve');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (11, 'Once');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (12, 'Doce');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (13, 'Trece');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (14, 'Catorce');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (15, 'Quince');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (16, 'Dieciseis');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (17, 'Diecisiete');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (18, 'Dieciocho');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (19, 'Diecinueve');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (21, 'Veintiun');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (22, 'Veintidos');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (23, 'Veintitres');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (24, 'Veinticuatro');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (25, 'Veinticinco');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (26, 'Veintiseis');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (27, 'Veintisiete');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (28, 'Veintiocho');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (29, 'Veintinueve');
+
+insert into NUMERO (ID_NRO, DESC_NRO)
+values (1000, 'Mil');
+
+prompt Done.
